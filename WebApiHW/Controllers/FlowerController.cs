f1e6d940d3f106f2581e51e9d199e0ea52d400e3
אור גנון <organon333@Gmail.com>
2022-05-15 23:20:54 +0300

    Add project files.

---
+++ WebApiHW/Controllers/FlowerController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiHW.Controllers
+{
+    [Route("api/[Action]")]
+    [ApiController]
+    public class FlowerController : ControllerBase
+    {
+        public string Rose()
+        {
+            return "Rose";
+        }
+        public string Lotus()
+        {
+            return "Lotus";
+        }
+        public string Jasmine()
+        {
+            return "Jasmine";
+        }
+        public string Sunflower()
+        {
+            return "SunFlower";
+        }
+    }
+
+}
